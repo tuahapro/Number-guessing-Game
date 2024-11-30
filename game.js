56c722411a646511c15e215e3bb0bc1a68b1288f
tuahapro <mohammadtuaha63@gmail.com>
2024-11-30 10:46:29 +0600

    solve problem

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 let playerOne = document.querySelector(".playerOne");
 let inputOne = document.querySelector(".inputOne");
 let btnOne = document.querySelector(".btn_1");
-let erorrMg = document.querySelector(".erorrMg")
-let PR_chance = document.querySelector(".PR_chance")
+let erorrMg = document.querySelector(".erorrMg");
+let PR_chance = document.querySelector(".PR_chance");
 
 let playerTwo = document.querySelector(".playerTwo");
 let PR_Two = document.querySelector(".PR_Two");
@@ -18,17 +18,18 @@ btnOne.addEventListener("click",()=>{
       
         
     }else if(isNaN(inputOne.value)){
-        erorrMg.innerHTML="Enter a number"
-        inputOne.value=""
+        erorrMg.innerHTML="Enter a number";
+        inputOne.value="";
        
     }else if(!(inputOne.value <=10 && inputOne.value >=0)){
-         erorrMg.innerHTML="Enter a Number 1 to 10"
+         erorrMg.innerHTML="Enter a Number 1 to 10";
+         inputOne.value="";
     }else{
        playerOne.style .display="none";
        playerTwo.style .display="inline-block";
        PR_chance.style .display="block";
-       PR_chance.innerHTML=`Chance: ${count}`
-       erorrMg.innerHTML=""
+       PR_chance.innerHTML=`Chance: ${count}`;
+       erorrMg.innerHTML="";
        
 
     }
@@ -36,32 +37,33 @@ btnOne.addEventListener("click",()=>{
 
 btnTwo.addEventListener("click",()=>{
     if(!inputTwo.value){
-      erorrMg1.innerHTML="Enter value asign"
+      erorrMg1.innerHTML="Enter value asign";
         
     }else if(isNaN(inputTwo.value)){
-        erorrMg1.innerHTML="Enter a number"
+        erorrMg1.innerHTML="Enter a number";
         inputTwo.value=""
     }else if(!(inputTwo.value <=10 && inputTwo.value >=0)){
-         erorrMg1.innerHTML="Enter a Number 1 to 10"
+         erorrMg1.innerHTML="Enter a Number 1 to 10";
+         inputTwo.value=""
     
     }else{
-         erorrMg1.innerHTML=""
+         erorrMg1.innerHTML="";
          
         if(count>1){
-            count--
-            PR_chance.innerHTML=`Chance: ${count}`
+            count--;
+            PR_chance.innerHTML=`Chance: ${count}`;
                 if(inputOne.value == inputTwo.value){
-                    PR_Two.innerHTML ="Player Two Winner"
-                    btnTwo.style.display="none"
+                    PR_Two.innerHTML ="Player Two Winner";
+                    btnTwo.style.display="none";
     
                  }
            
         }else{
             count=0;
-            PR_chance.innerHTML=`Chance: ${count}`
-            PR_Two.innerHTML ="Player One Winner"
-             btnTwo.style.display="none"
-              inputTwo.value=""
+            PR_chance.innerHTML=`Chance: ${count}`;
+            PR_Two.innerHTML ="Player One Winner";
+             btnTwo.style.display="none";
+              inputTwo.value="";
 
         }
     }
